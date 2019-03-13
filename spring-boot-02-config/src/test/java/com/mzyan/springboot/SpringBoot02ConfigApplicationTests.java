d4edcc6b9a0672ecc439ee72596ed3cca72a1ddc
mzyan <mengzhao.yan@foxmail.com>
2019-03-13 11:08:13 +0800

    类获取yml配置文件中的值

--- spring-boot-02-config/src/test/java/com/mzyan/springboot/SpringBoot02ConfigApplicationTests.java
+++ spring-boot-02-config/src/test/java/com/mzyan/springboot/SpringBoot02ConfigApplicationTests.java
@@ -1,16 +1,28 @@
 package com.mzyan.springboot;
 
+import com.mzyan.springboot.bean.Person;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+/**
+ * SpringBoot单元测试
+ *
+ * 可以在测试期间很方便地类似编码一样进行自动注入容器的功能
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringBoot02ConfigApplicationTests {
 
+    @Autowired
+    Person person;
+
+
     @Test
     public void contextLoads() {
+        System.out.println(person);
     }
 
 }
