359aa09fdc53f6a9aa9a327fb8db326e38df1458
mzyan <mengzhao.yan@foxmail.com>
2019-03-16 15:03:12 +0800

    实现员工删除功能

--- spring-boot-04-web-restfulcrud/src/main/java/com/mzyan/springboot/controller/EmployeeController.java
+++ spring-boot-04-web-restfulcrud/src/main/java/com/mzyan/springboot/controller/EmployeeController.java
@@ -7,10 +7,7 @@ import com.mzyan.springboot.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
@@ -79,4 +76,9 @@ public class EmployeeController {
         return "redirect:/emps";
     }
 
+    @DeleteMapping("/emp/{id}")
+    public String deleteEmployee(@PathVariable("id") Integer id){
+        employeeDao.delete(id);
+        return "redirect:/emps";
+    }
 }
