dd809697ea3e202f71051df80d1b33d2bdd90eb8
mzyan <mengzhao.yan@foxmail.com>
2019-03-18 17:13:01 +0800

    创建项目，编写自动配置组件

---
+++ spring-boot-08-starter/mzyan-spring-boot-starter-autoconfigurer/src/main/java/com/mzyan/starter/HelloServiceAutoConfiguration.java
@@ -0,0 +1,22 @@
+package com.mzyan.starter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConditionalOnWebApplication // web应用情况下
+@EnableConfigurationProperties(HelloProperties.class)
+public class HelloServiceAutoConfiguration {
+
+    @Autowired
+    HelloProperties helloProperties;
+    @Bean
+    public HelloService helloService(){
+        HelloService service = new HelloService();
+        service.setHelloProperties(helloProperties);
+        return service;
+    }
+}
