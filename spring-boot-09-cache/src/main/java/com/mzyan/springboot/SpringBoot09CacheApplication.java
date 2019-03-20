94d5ea747f3adabe326bb776586ab5dc25aa3ace
mzyan <mengzhao.yan@foxmail.com>
2019-03-20 16:35:41 +0800

    整合redis 并测试相应的读写功能

--- spring-boot-09-cache/src/main/java/com/mzyan/springboot/SpringBoot09CacheApplication.java
+++ spring-boot-09-cache/src/main/java/com/mzyan/springboot/SpringBoot09CacheApplication.java
@@ -6,16 +6,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 
 /**
- * 搭建基本环境
+ * 一、搭建基本环境
  * 1、导入数据库文件，创建出department和employee表
  * 2、创建javabean封装数据
  * 3、整合MyBatis操作数据库
  *      1）配置数据源信息
  *      2）使用注解版的MyBatis
  *          1. @MapperScan 指定需要扫描的mapper接口所在的包
- * 快速体验缓存
+ * 二、快速体验缓存
  * 1、开启基于注解的缓存
  * 2、标注缓存注解
+ *
+ * 三、整合redis作为缓存
+ *      Redis是一个开源（BSD许可）的，内存中的数据结构存储系统，它可以作为数据库、缓存和消息中间件
+ *  1、安装redis，使用docker
+ *  2、引入redis的starter
+ *  3、配置redis
  */
 @MapperScan("com.mzyan.springboot.mapper")
 @SpringBootApplication
--- spring-boot-09-cache/src/test/java/com/mzyan/springboot/SpringBoot09CacheApplicationTests.java
+++ spring-boot-09-cache/src/test/java/com/mzyan/springboot/SpringBoot09CacheApplicationTests.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -14,6 +16,49 @@ public class SpringBoot09CacheApplicationTests {
 
     @Autowired
     EmployeeMapper employeeMapper;
+
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;   // <k, v>都是字符串的
+
+    @Autowired
+    RedisTemplate redisTemplate;    //<k, v> 都是对象的
+
+    @Autowired
+    RedisTemplate<Object, Employee> empRedisTemplate;
+
+    /**
+     * Redis 常见的五大数据类型：
+     *      String（字符串）、List（列表）、Set（集合）、Hash（散列）、ZSet（有序集合）
+     *   stringRedisTemplate.opsForValue()[String（字符串）]
+     *   stringRedisTemplate.opsForList()[List（列表）]
+     *   stringRedisTemplate.opsForSet()[Set（集合）]
+     *   stringRedisTemplate.opsForHash()[Hash（散列）]
+     *   stringRedisTemplate.opsForZSet()[ZSet（有序集合）]
+     *
+     */
+    @Test
+    public void test01(){
+        // 给redis中保存数据
+//        stringRedisTemplate.opsForValue().append("msg", "hello");
+//        String msg = stringRedisTemplate.opsForValue().get("msg");
+//        System.out.println(msg);
+
+        stringRedisTemplate.opsForList().leftPush("mylist","1");
+        stringRedisTemplate.opsForList().leftPush("mylist","2");
+    }
+
+    // 测试保存对象
+    @Test
+    public void test02(){
+        Employee emp = employeeMapper.getEmployeeById(1);
+        //默认如果保存对象，使用jdk序列化机制，序列化后的数据保存到redis
+//        redisTemplate.opsForValue().set("emp-01", emp);
+        // 1、将数据以json的方式保存
+        //   1. 自己将对象保存为json
+        //   2. redisTemplate默认的序列化规则, 改变默认的序列化规则
+        empRedisTemplate.opsForValue().set("emp-01", emp);
+    }
+
 
     @Test
     public void contextLoads() {
